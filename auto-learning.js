199509302a4e01574fa554084fae739909c7ce44
kay wang <kay_wang@126.com>
2020-12-30 14:02:06 +0800

    original file

---
+++ auto-learning.js
@@ -0,0 +1,15 @@
+if (document.location.origin === 'http://edu.inspur.com') {
+  console.log('I am learning!')
+  setInterval(function () {
+    RemoveWarningHtml && RemoveWarningHtml();
+    console.log('Remove waning, auto learning now!')
+  }, 10*1000)
+}
+
+
+
+
+setInterval(function () {
+    RemoveWarningHtml && RemoveWarningHtml();
+    console.log('Remove waning, auto learning now!')
+  }, 10*1000)
